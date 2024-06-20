51b2297861838f21f72b52ac06c87ebd32edfbec
Soham2912 <sohamlahoti@gmail.com>
2024-06-20 23:23:28 +0530

    continuous commit

---
+++ setInterval.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+setInterval() {
+    local interval=$1
+    shift
+    local command="$@"
+
+    while true; do
+        eval $command
+        sleep $interval
+    done
+}
+
+
+yourCode() {
+   
+message=" continuous commit"
+git add .
+git commit -m "$message"
+git push
+
+echo "Done"
+}
+
+
+setInterval 5 yourCode
