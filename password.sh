2bfda466ce066e7133657a83d97c25d5d955c73e
Soham2912 <sohamlahoti@gmail.com>
2024-06-27 04:36:25 +0530

    Added bash script to generate random passwords of the given length

---
+++ password.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+length=12
+
+characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
+
+password=$(head /dev/urandom | tr -dc "$characters" | head -c "$length")
+
+
+echo "Generated Password: $password"
